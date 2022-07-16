aaf5cc59f51bb3a7c6f0a37d5a48b5ae36f2f51f
Bucha1958 <okoriemmadu@gmail.com>
2022-07-16 04:18:59 +0100

    Bring

---
+++ 0x01-my_coding_challenge/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - function that print strings with puts
+ * fumction
+ *
+ * Return : Always 0
+ *
+ */
+int main(void)
+{
+  puts("\"programming is like building a multilingual puzzle\n");
+  return (0);
+}
